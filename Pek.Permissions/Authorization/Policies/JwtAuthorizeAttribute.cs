f63a06531bc8f47e5ee1262127c5180e130bfbe1
猿人易 <qcjxberin@outlook.com>
2024-09-25 20:43:33 +0800

    添加编辑器配置、GitHub Actions 工作流及 JWT 授权类
    在 `.editorconfig` 文件中添加了编辑器配置，指定了缩进样式、编码格式、行尾符、代码样式等设置。
    在 `.gitattributes` 文件中添加了默认的行尾规范化行为、命令提示符 diff 的默认行为、项目和解决方案文件的合并驱动程序设置、图像文件的处理行为以及常见文档格式的 diff 行为。
    在 `.gitignore` 文件中添加了忽略 Visual Studio 生成的文件和目录的规则。
    在 `LICENSE` 文件中添加了 MIT 许可证的内容，声明了版权信息和许可条款。
    在 `NuGet.config` 文件中添加了 NuGet 包源配置，指定了 Azure 和 nuget.org 的包源地址。
    在 `Pek.Permissions.sln` 文件中添加了 Visual Studio 解决方案文件，包含了一个名为 `Pek.Permissions` 的项目和一些解决方案项。
    在 `Readme.MD` 文件中添加了项目的简要介绍，说明了这是一个 JWT 权限类库。
    在 `publish-beta.yml` 文件中添加了 GitHub Actions 工作流配置，用于在推送到 `main` 分支时构建和发布 beta 版本的 NuGet 包。
    在 `publish.yml` 文件中添加了 GitHub Actions 工作流配置，用于在推送带有 `v*` 标签时构建和发布正式版本的 NuGet 包。
    
    增加了 `Pek.Permissions.csproj` 文件，配置了项目的基本信息、生成选项、包引用和资源文件。
    新增了多个文件，定义了 JWT 授权相关的类和接口，包括 `AuthorizeResult`、`IJsonWebTokenCustomerAuthorizeOption`、`JsonWebTokenCustomerAuthorizeMiddleware`、`JsonWebTokenCustomerAuthorizeOption`、`IJsonWebTokenAuthorizationRequirement`、`JsonWebTokenAuthorizationHandler`、`JsonWebTokenAuthorizationRequirement`、`JwtAuthorizeAttribute`、`Extensions.Service` 等。
    
    添加了 `IdentityErrorChineseDescriber` 类，用于提供中文的身份验证错误描述。
    添加了多个扩展类和接口，用于处理用户标识、JWT 令牌创建和校验、设备令牌绑定信息、授权类型常量等。
    
    新增了多个文件，定义了 JWT 令牌的创建、存储和校验功能，包括 `JsonWebTokenBuilder`、`JsonWebTokenStore`、`JsonWebTokenValidator`、`TokenPayloadStore` 等。
    新增了多个配置类，用于配置登录锁定选项、密码选项、权限选项、登录选项、存储选项、用户选项等。
    
    在 `SecurityResources.Designer.cs` 和 `SecurityResources.resx` 文件中添加了新的本地化资源，包括各种错误消息和验证消息。

---
+++ Pek.Permissions/Authorization/Policies/JwtAuthorizeAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace DH.Permissions.Authorization.Policies;
+
+/// <summary>
+/// Jwt授权
+/// </summary>
+public class JwtAuthorizeAttribute : AuthorizeAttribute
+{
+}
