a75256875424b8df273d052a3933b3fbf6100a52
猿人易 <qcjxberin@outlook.com>
2024-11-23 14:10:43 +0800

    更新依赖版本并改进代码一致性
    在 `publish-beta.yml` 和 `publish.yml` 文件中，将 `actions/setup-dotnet` 的版本从 `v3` 更新到 `v4`，并将 `dotnet-version` 从 `8.x` 扩展为 `8.x` 和 `9.x`。
    在 `JwtDemo.csproj` 文件中，将 `DH.NCore` 包的版本从 `3.91.2024.1013-beta0805` 更新到 `4.0.2024.1119-beta0422`。
    在 `IJsonWebTokenBuilder.cs` 文件中，将所有 `string` 类型改为 `String` 类型。
    在 `Pek.Permissions.csproj` 文件中，将目标框架从 `net8.0` 更新到 `net9.0`。
    在 `Pek.Permissions.csproj` 文件中，将 `Pek.AspNetCore` 包的版本从 `3.91.2024.1028-beta0841` 更新到 `4.0.2024.1123-beta0350`，并将 `Pek.Common` 包的版本从 `3.91.2024.1026-beta1447` 更新到 `4.0.2024.1123-beta0424`。

--- Pek.Permissions/Identity/JwtBearer/IJsonWebTokenBuilder.cs
+++ Pek.Permissions/Identity/JwtBearer/IJsonWebTokenBuilder.cs
@@ -11,40 +11,41 @@ public interface IJsonWebTokenBuilder
     /// 创建令牌
     /// </summary>
     /// <param name="payload">负载</param>
-    JsonWebToken Create(IDictionary<string, string> payload);
+    JsonWebToken Create(IDictionary<String, String> payload);
 
     /// <summary>
     /// 创建令牌
     /// </summary>
     /// <param name="payload">负载</param>
     /// <param name="options">Jwt选项配置</param>
-    JsonWebToken Create(IDictionary<string, string> payload, JwtOptions options);
+    JsonWebToken Create(IDictionary<String, String> payload, JwtOptions options);
 
     /// <summary>
     /// 刷新令牌
     /// </summary>
     /// <param name="refreshToken">刷新令牌</param>
-    JsonWebToken Refresh(string refreshToken);
+    JsonWebToken Refresh(String refreshToken);
 
     /// <summary>
     /// 刷新令牌
     /// </summary>
     /// <param name="refreshToken">刷新令牌</param>
-    JsonWebToken Refresh(string refreshToken, JwtOptions options);
+    /// <param name="options"></param>
+    JsonWebToken Refresh(String refreshToken, JwtOptions options);
 
     /// <summary>
     /// 刷新令牌
     /// </summary>
     /// <param name="refreshToken">刷新令牌</param>
     /// <param name="RefreshExpireMinutes">刷新令牌有效期分钟数</param>
-    JsonWebToken Refresh(string refreshToken, Double RefreshExpireMinutes);
+    JsonWebToken Refresh(String refreshToken, Double RefreshExpireMinutes);
 
     /// <summary>
     /// 刷新令牌，延时清理数据
     /// </summary>
     /// <param name="refreshToken">刷新令牌</param>
     /// <param name="expire">延时时间。秒</param>
-    JsonWebToken Refresh(string refreshToken, Int32 expire);
+    JsonWebToken Refresh(String refreshToken, Int32 expire);
 
     /// <summary>
     /// 刷新令牌，延时清理数据
@@ -52,5 +53,5 @@ public interface IJsonWebTokenBuilder
     /// <param name="refreshToken">刷新令牌</param>
     /// <param name="expire">延时时间。秒</param>
     /// <param name="RefreshExpireMinutes">刷新令牌有效期分钟数</param>
-    JsonWebToken Refresh(string refreshToken, Int32 expire, Double RefreshExpireMinutes);
+    JsonWebToken Refresh(String refreshToken, Int32 expire, Double RefreshExpireMinutes);
 }
