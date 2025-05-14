11cc3dd1f36da87ec4dc089e852124a82c0529d0
猿人易 <qcjxberin@outlook.com>
2025-05-14 16:00:38 +0800

    统一参数类型并新增 JWT 授权特性
    在 `IJsonWebTokenStore.cs` 文件中，所有方法的参数类型从 `string` 更改为 `String`，以保持一致性和风格。具体包括 `GetRefreshToken`、`RemoveRefreshToken`、`GetToken`、`RemoveToken`、`ExistsToken`、`BindUserDeviceToken` 和 `GetUserDeviceToken` 方法的参数类型。
    
    在 `JwtAuthorizeAttribute.cs` 文件中，新增了一个自定义的 `JwtAuthorizeAttribute` 类。该类继承自 `AuthorizeAttribute`，用于支持同一系统中多个 JWT 的授权需求。新增的类包含一个 `From` 属性，用于标识 JWT 来源，并在构造函数中初始化该属性和设置策略为 "jwt"。

--- Pek.Permissions/Identity/JwtBearer/IJsonWebTokenStore.cs
+++ Pek.Permissions/Identity/JwtBearer/IJsonWebTokenStore.cs
@@ -11,7 +11,7 @@ public interface IJsonWebTokenStore
     /// 获取刷新令牌
     /// </summary>
     /// <param name="token">刷新令牌</param>
-    RefreshToken GetRefreshToken(string token);
+    RefreshToken GetRefreshToken(String token);
 
     /// <summary>
     /// 保存刷新令牌
@@ -23,33 +23,33 @@ public interface IJsonWebTokenStore
     /// 移除刷新令牌
     /// </summary>
     /// <param name="token">刷新令牌</param>
-    void RemoveRefreshToken(string token);
+    void RemoveRefreshToken(String token);
 
     /// <summary>
     /// 延时移除刷新令牌
     /// </summary>
     /// <param name="token">刷新令牌</param>
     /// <param name="expire">延时时间。秒</param>
-    void RemoveRefreshToken(string token, Int32 expire);
+    void RemoveRefreshToken(String token, Int32 expire);
 
     /// <summary>
     /// 获取访问令牌
     /// </summary>
     /// <param name="token">访问令牌</param>
-    JsonWebToken GetToken(string token);
+    JsonWebToken GetToken(String token);
 
     /// <summary>
     /// 移除访问令牌
     /// </summary>
     /// <param name="token">访问令牌</param>
-    void RemoveToken(string token);
+    void RemoveToken(String token);
 
     /// <summary>
     /// 延时移除访问令牌
     /// </summary>
     /// <param name="token">访问令牌</param>
     /// <param name="expire">延时时间。秒</param>
-    void RemoveToken(string token, Int32 expire);
+    void RemoveToken(String token, Int32 expire);
 
     /// <summary>
     /// 保存访问令牌
@@ -62,7 +62,7 @@ public interface IJsonWebTokenStore
     /// 是否存在访问令牌
     /// </summary>
     /// <param name="token">访问令牌</param>
-    bool ExistsToken(string token);
+    bool ExistsToken(String token);
 
     /// <summary>
     /// 绑定用户设备令牌
@@ -71,12 +71,12 @@ public interface IJsonWebTokenStore
     /// <param name="clientType">客户端类型</param>
     /// <param name="info">设备信息</param>
     /// <param name="expires">过期时间</param>
-    void BindUserDeviceToken(string userId, string clientType, DeviceTokenBindInfo info, DateTime expires);
+    void BindUserDeviceToken(String userId, String clientType, DeviceTokenBindInfo info, DateTime expires);
 
     /// <summary>
     /// 获取用户设备令牌
     /// </summary>
     /// <param name="userId">用户标识</param>
     /// <param name="clientType">客户端类型</param>
-    DeviceTokenBindInfo GetUserDeviceToken(string userId, string clientType);
+    DeviceTokenBindInfo GetUserDeviceToken(String userId, String clientType);
 }
---
+++ Pek.Permissions/JwtAuthorizeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Pek.Permissions;
+
+/// <summary>
+/// 自定义 Jwt 授权特性。
+/// 用于指定来源，支持同一系统多个 Jwt 的授权需求。
+/// </summary>
+public class JwtAuthorizeAttribute : AuthorizeAttribute
+{
+    /// <summary>
+    /// 来源，方便同一系统多个Jwt使用
+    /// </summary>
+    public String From { get; set; }
+
+    /// <summary>
+    /// 初始化 <see cref="JwtAuthorizeAttribute"/> 类的新实例。
+    /// </summary>
+    /// <param name="from">Jwt 来源标识。</param>
+    public JwtAuthorizeAttribute(String from) { From = from; Policy = "jwt"; }
+}
